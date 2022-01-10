4578aa615c58bab9601cfdf4ab615e1d38448120
ananas111 <pedorenko.anastasiia@gmail.com>
2022-01-10 23:34:41 +0200

    'added 'other brands'

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import requests
 from config import API_KEY, token
 
 bot = telebot.TeleBot(token)
-
 api_link_ria = 'https://developers.ria.com/auto/search?api_key=' + API_KEY + "&category_id=1"
-
-
-
-
 
 
 @bot.message_handler(commands=['insta'])
@@ -17,14 +12,6 @@ def instagram(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("Перейти в Инстаграм", url="https://www.instagram.com/itproger_official/"))
     bot.send_message(message.chat.id, "Нажмите на кнопку ниже и погрузитесь в мир IT прямо сейчас",
-                     parse_mode='html', reply_markup=markup)
-
-
-@bot.message_handler(commands=['vk'])
-def vk(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Посетить группу ВК", url="https://itproger.com"))
-    bot.send_message(message.chat.id, "ЧТоб войти в группу вк просто нажми на кнопку",
                      parse_mode='html', reply_markup=markup)
 
 
@@ -37,7 +24,7 @@ def start(message):
     btn4 = types.KeyboardButton('Honda')
     btn5 = types.KeyboardButton('Ford')
     btn6 = types.KeyboardButton('Lexus')
-    btn7 = types.KeyboardButton('Другие: введите марку автомобиля')
+    btn7 = types.KeyboardButton('Другие')
     markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7)
     send_mess = f"<b> Привет {message.from_user.first_name} {message.from_user.last_name} " \
                 f"</b>!\nКакая модель тебя интересует?"
@@ -47,16 +34,13 @@ def start(message):
 def get_brand_id(brand):
     brands_api = 'https://developers.ria.com/auto/categories/1/marks?api_key=' + API_KEY
     brands_list = requests.get(brands_api).json()
+    print(brands_list)
     for i in range(len(brands_list)):
         if brands_list[i]['name'] == brand:
             brand_id = brands_list[i]['value']
             return brand_id
         else:
             i += 1
-
-
-list1 = get_brand_id('Audi')
-print(list1)
 
 
 def add_paramethers(param):
@@ -70,10 +54,6 @@ def get_model_id(brand_id):
     filter_search = requests.get(api_link_ria + param).json()
     models = filter_search['result']['search_result']['ids']
     return models
-
-
-model_new = get_model_id(6)
-print(model_new)
 
 
 @bot.message_handler(content_types=['text'])
@@ -139,32 +119,28 @@ def mess(message):
             i += 1
             bot.send_message(message.chat.id, f"{model_link}")
 
-    elif get_message_bot == 'Другие: введит' \
-                            'е марку автомобиля':
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("введит' \
-                            'е марку автомобиля"))
-        brand_id = get_brand_id(message)
+    elif get_message_bot == "Другие":
+        bot.send_message(message.chat.id, 'Введите название модели автомобиля')
+        bot.register_next_step_handler(message, other)
+
+
+@bot.message_handler(content_types=['text'])
+def other(message):
+    get_message_bot = message.text.strip()
+    try:
+        brand = get_message_bot
+        brands_api = 'https://developers.ria.com/auto/categories/1/marks?api_key=' + API_KEY
+        brands_list = requests.get(brands_api).json()
+        brand_id = get_brand_id(brand)
         models = get_model_id(brand_id)
-        for i in range(2):
+        for i in range(len(models)):
             model_api = 'https://developers.ria.com/auto/info?api_key=' + API_KEY + '&auto_id=' + models[i]
             model = requests.get(model_api).json()
             model_link = 'https://auto.ria.com/uk' + model['linkToView']
             i += 1
-            bot.send_message(message.chat.id, f"{model_link}")
-    elif get_message_bot == 'под мообильные телефоны':
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Посмотреть курсы по Unity", url="https://itproger.com/tag/unity"))
-
-    else:
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-        btn1 = types.KeyboardButton('Создание игр')
-        btn2 = types.KeyboardButton('Мобильные приложения')
-        btn3 = types.KeyboardButton('Веб разработка')
-        btn4 = types.KeyboardButton('Софт для компьютеров')
-        btn5 = types.KeyboardButton('Обработка данных')
-        btn6 = types.KeyboardButton('Создание ИИ')
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+            bot.send_message(message.chat.id, f"{model_link}", brands_list)
+    except Exception:
+        bot.send_message(message.from_user.id, 'Вы ввели данные в неправильном формате.')
 
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, interval=0)
